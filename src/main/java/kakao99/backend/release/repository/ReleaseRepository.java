2a67e23d5b1f6955300a8c79943b2d1421a3dcb7
Haebin Ahn <gnh06280@naver.com>
2023-07-06 23:01:20 +0900

    [style] 시작

--- src/main/java/kakao99/backend/release/repository/ReleaseRepository.java
+++ src/main/java/kakao99/backend/release/repository/ReleaseRepository.java
@@ -1,7 +1,14 @@
 package kakao99.backend.release.repository;
 
+import kakao99.backend.entity.ReleaseNote;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface ReleaseRepository {
+public interface ReleaseRepository extends JpaRepository<ReleaseNote, Long> {
+    ReleaseNote save(ReleaseNote releaseNote);
+
+    //Optional<ReleaseNote> findReleaseNoteByProject(Long id); 프로젝트 아이디로 탐색해야 하는데 어떻게?
 }
