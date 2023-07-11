65db0c79c76645907558169a705558c4e9311f48
jedo0224 <qkrwotjr0@gmail.com>
2023-07-11 11:56:10 +0900

    [feat] 전체 이슈 조회 기능 추가

---
+++ src/main/java/kakao99/backend/issue/dto/IssueDTO.java
@@ -0,0 +1,43 @@
+package kakao99.backend.issue.dto;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ManyToOne;
+import kakao99.backend.entity.Member;
+import kakao99.backend.entity.Memo;
+import lombok.*;
+import net.minidev.json.annotate.JsonIgnore;
+
+import java.util.Date;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
+public class IssueDTO {
+
+    private Long id;
+    private Integer issueNum;
+    private String title;
+    private String issueType;
+    private String description;
+    private String status;
+    private Integer listPosition;
+    private String file;
+    private Date createdAt;
+
+    ////    @JsonIgnore
+    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+    @JsonIgnore
+//    @ManyToOne(fetch = FetchType.LAZY)
+    private Member memberIdInCharge;
+//    private Long memberIdReport;
+//    private List<Memo> comments;
+
+    public IssueDTO() {
+
+    }
+}
+
