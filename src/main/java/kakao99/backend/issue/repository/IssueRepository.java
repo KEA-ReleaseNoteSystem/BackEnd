07868f87999127733cbb84a1b136717f2732141f
Doyoung <doodoo55@gachon.ac.kr>
2023-07-20 14:59:15 +0900

    [style] issue 도메인 코드 리팩토링 완료

--- src/main/java/kakao99/backend/issue/repository/IssueRepository.java
+++ src/main/java/kakao99/backend/issue/repository/IssueRepository.java
@@ -18,13 +18,8 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface IssueRepository extends JpaRepository<Issue, Long> {
+public interface IssueRepository extends JpaRepository<Issue, Long>, IssueRepositoryCustom {
     Issue save(Issue issue);
-
-//    Issue findByProjectIdAndIssueId(Long projectId, Long issueId);
-
-//    @Query("select m from Issue m join fetch m.project")
-//    List<Issue> findAllByProjectId(Long projectId);
 
     @Query("select m from Issue m join fetch m.project join fetch m.memberInCharge join fetch m.memberReport where m.project.id=:projectId and m.isActive = true")
     List<Issue> findAllByProjectId(@Param("projectId") Long projectId);
@@ -89,7 +84,7 @@ public interface IssueRepository extends JpaRepository<Issue, Long> {
 
     // 프로젝트 내의 releaseNote에 포함되지 않은 이슈들 조회
 @   Query("select m from Issue m join fetch m.memberInCharge join fetch m.memberReport where m.releaseNote.id = null and m.isActive = true and m.project.id =:projectId")
-List<Issue> findAllByNotReleaseNoteId(@Param("projectId") Long projectId);
+    List<Issue> findAllByNotReleaseNoteId(@Param("projectId") Long projectId);
 
     @Modifying
     @Transactional
@@ -100,7 +95,6 @@ List<Issue> findAllByNotReleaseNoteId(@Param("projectId") Long projectId);
     @Transactional
     @Query("UPDATE Issue m SET m.releaseNote.id =:releaseNoteId  where m.id =:issueId")
     int insertIssueFromReleaseNote(@Param("releaseNoteId") Long releaseNoteId, @Param("issueId") Long issueId);
-
 
 
 }
--- src/main/java/kakao99/backend/issue/repository/IssueStatus.java
+++
@@ -1,5 +0,0 @@
-package kakao99.backend.issue.repository;
-
-public enum IssueStatus {
-    BACKLOG,INPROGRESS, DONE
-}
