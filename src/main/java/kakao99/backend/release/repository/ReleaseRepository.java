e286e8d944ea1172d67d4e0ea7dcee35ff7e91aa
KangDeok <rkdejr2321@naver.com>
2023-08-11 11:18:55 +0900

    [feat] 릴리즈 노트 조회 페이지네이션

--- src/main/java/kakao99/backend/release/repository/ReleaseRepository.java
+++ src/main/java/kakao99/backend/release/repository/ReleaseRepository.java
@@ -2,6 +2,8 @@ package kakao99.backend.release.repository;
 
 import kakao99.backend.entity.Project;
 import kakao99.backend.entity.ReleaseNote;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
